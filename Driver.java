d858acf34fc0c5e743a9d3bf28ddcd7f349b332a
uatragada <42359216+uatragada@users.noreply.github.com>
2023-04-17 22:07:49 -0400

    Script + Driver Menu

--- Driver.java
+++ Driver.java
@@ -23,9 +23,65 @@ public class Driver {
         Scanner kbd = new Scanner(System.in);
         ArrayList<String> validate = new ArrayList<String>();
         String input = "";
-        switch(input)
+        Boolean exit = false;
+        int topLevel = 0;
+        int bottomLevel = 0;
+        String userID = null;
+        while(!exit)
         {
-            case "":
+            /**
+             * Displays the main menu options for the user to select from.
+             * If the user enters an invalid operation, they will be prompted to enter a valid one.
+             */
+            if(topLevel == 0 && bottomLevel == 0) //TOP LEVEL MENU
+            {
+                System.out.println("<-----LOGIN/REGISTER----->");
+                System.out.println("1. Create Profile");
+                System.out.println("2. Login");
+                System.out.println("3. Delete Profile");
+                System.out.print("Input:");
+
+                topLevel = Integer.parseInt(kbd.next());
+                while(topLevel <= 0)
+                {
+                    System.out.println("PLEASE ENTER A VALID OPERATION:");
+                    topLevel = Integer.parseInt(kbd.next());
+                }
+            }
+            else if(topLevel == 1 && bottomLevel == 0) //CREATING A USER PROFILE
+            {
+                System.out.println("<-----CREATE PROFILE----->");
+                System.out.print("Name: ");
+                String name = kbd.next();
+                System.out.print("Email: ");
+                String userEmail = kbd.next();
+                System.out.print("Password: ");
+                String userPass = kbd.next();
+                System.out.print("DOB: ");
+                String dob = kbd.next();
+
+                topLevel = 0; //PUSH USER BACK TO TOP LEVEL MENU
+
+            }
+            else if(topLevel == 2 && bottomLevel == 0) //LOGIN
+            {
+                System.out.println("<-----CREATE PROFILE----->");
+                System.out.print("Email: ");
+                String userEmail = kbd.next();
+                System.out.print("Password: ");
+                String userPass = kbd.next();
+                //LOG IN USER AND SET USERID TO USERID
+            }
+            else if(topLevel == 3 && bottomLevel == 0)
+            {
+                System.out.println("<-----DELETE PROFILE----->");
+                System.out.print("Email: ");
+                String userEmail = kbd.next();
+            }
+            
+            
+            
         }
+        
     }
 }
---
+++ run.bat
@@ -0,0 +1,3 @@
+
+javac -cp ".;postgresql-42.6.0.jar" *.java
+java -cp ".;postgresql-42.6.0.jar" Driver 
