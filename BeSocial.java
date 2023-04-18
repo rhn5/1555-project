258ea2b0ce47fe4d6e6aee78506069aa85876fde
CHB213 <112492924+CHB213@users.noreply.github.com>
2023-04-17 23:04:38 -0400

    Add files via upload

--- BeSocial.java
+++ BeSocial.java
@@ -1,25 +1,21 @@
-import java.util.*;
-
-import javax.naming.spi.DirStateFactory.Result;
+import java.util.Scanner;
 
 import java.sql.*;
-import java.text.ParseException;
-import java.time.*;
 import java.lang.*;
 
 public class BeSocial {
 
     private Connection connection;
-	private Statement statement;
-	private String url;
-	private String username;
-	private String password;
+    private Statement statement;
+    private String url;
+    private String username;
+    private String password;
 
     public BeSocial() {
         Scanner input = new Scanner(System.in);
         System.out.println("<-----LOG INTO DB----->");
-        
-    
+
+
         /**
          * Prompts the user for a URL, username, and password to connect to a database.
          * If the URL is "localhost", it is converted to a PostgreSQL URL.
@@ -31,71 +27,53 @@ public class BeSocial {
         {
             url = "jdbc:postgresql://localhost:5432/";
         }
-        
+
         System.out.print("Username:");
         username = input.next();
-        
+
         System.out.print("Password:");
         password = input.next();
 
-        try{
+        try {
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(url, username, password);
-        }
-        catch(Exception e){
+        } catch(Exception e){
             e.printStackTrace();
         }
     }
 
-    public int createProfile(String name, String email, String password, LocalDate dateOfBirth) {
+    public userInstance createProfile(String name, String email, String password, Date dateOfBirth) throws SQLException {
         String count = "SELECT COUNT(*) FROM profiles";
         ResultSet rs = statement.executeQuery(count);
-        rs.next();
-        int userId = rs.getInt(1);
-        System.out.println(userId);
+
+        int userID = rs.getInt(1);
+        System.out.println(userID);
 
         String query = "INSERT INTO profiles (userID, name, email, password, dateOfBirth) " + "VALUES (?, ?, ?, ?, ?)";
-        PreparedStatement st;
-        try{
-            st = connection.prepareStatement(query);
-        }
-        catch(SQLException s){
-            System.out.println();
-        }
-        
-        Date sql_date = new Date (dateOfbirth.getYear() ,dateOfbirth.getMonth(), dateOfbirth.getDay())
-        
 
-        
-        st.setInt(1, userId);
+        PreparedStatement st = connection.prepareStatement(query);
+
+        Date sql_date = new Date (dateOfBirth.getYear() ,dateOfBirth.getMonth(), dateOfBirth.getDay());
+
+        st.setInt(1, userID);
         st.setString(2, name);
         st.setString(3, email);
         st.setString(4, password);
-        st.setDate(5, dateOfBirth.toString());
+        st.setDate(5, sql_date);
 
         rs = st.executeQuery();
         if(rs.next()){
-            return 1;
+            return new userInstance(userID);
         }
         else{
             System.out.println();
-            return -1;
+            return null;
         }
     }
 
-    public int dropProfile(String email) {
-        return 1;
-    }
-
-    public int login(String email, String password) {
+    public int login(String email, String password) throws SQLException {
         String query = "SELECT * FROM profiles WHERE email=? AND password=?";
-        PreparedStatement st;// = connection.prepareStatement(query);
-        try{
-            st = connection.prepareStatement(query);
-        }
-        catch(SQLException s){
-            System.out.println();
-        }
+        PreparedStatement st = connection.prepareStatement(query);
 
         st.setString(1,email);
         st.setString(2,password);
@@ -103,76 +81,86 @@ public class BeSocial {
         return 1;
     }
 
-    public int initiateFriendship(String userID) {
-        return 1;
-    }
+    private class userInstance {
+        int userID;
+        private userInstance(int userID) {
+            this.userID = userID;
+        }
 
-    public int confirmFriendRequests() {
-        return 1;
-    }
+        public int dropProfile() {
+            return 1;
+        }
 
-    public int createGroup() {
-        return 1;
-    }
+        public int initiateFriendship(String userID) {
+            return 1;
+        }
 
-    public int initiateAddingGroup() {
-        return 1;
-    }
+        public int confirmFriendRequests() {
+            return 1;
+        }
 
-    public int confirmGroupMembership() {
-        return 1;
-    }
+        public int createGroup() {
+            return 1;
+        }
 
-    public int leaveGroup() {
-        return 1;
-    }
+        public int initiateAddingGroup() {
+            return 1;
+        }
 
-    public int searchForProfile() {
-        return 1;
-    }
+        public int confirmGroupMembership() {
+            return 1;
+        }
 
-    public int sendMessageToUser() {
-        return 1;
-    }
+        public int leaveGroup() {
+            return 1;
+        }
 
-    public int sendMessageToGroup() {
-        return 1;
-    }
+        public int searchForProfile() {
+            return 1;
+        }
 
-    public int displayMessages() {
-        return 1;
-    }
+        public int sendMessageToUser() {
+            return 1;
+        }
 
-    public int displayNewMessages() {
-        return 1;
-    }
+        public int sendMessageToGroup() {
+            return 1;
+        }
 
-    public int displayFriends() {
-        return 1;
-    }
+        public int displayMessages() {
+            return 1;
+        }
 
-    public int rankGroups() {
-        return 1;
-    }
+        public int displayNewMessages() {
+            return 1;
+        }
 
-    public int rankProfiles() {
-        return 1;
-    }
+        public int displayFriends() {
+            return 1;
+        }
 
-    public int topMessages() {
-        return 1;
-    }
+        public int rankGroups() {
+            return 1;
+        }
 
-    public int threeDegrees() {
-        return 1;
-    }
+        public int rankProfiles() {
+            return 1;
+        }
 
-    public int logout() {
-        return 1;
-    }
+        public int topMessages() {
+            return 1;
+        }
 
-    public int exit() {
-        return 1;
-    }
+        public int threeDegrees() {
+            return 1;
+        }
 
+        public int logout() {
+            return 1;
+        }
+
+        public int exit() {
+            return 1;
+        }
+    }
 }
