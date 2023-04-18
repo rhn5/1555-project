bd765ecf21962f34da3c9fec15b24556e8f84aed
uatragada <42359216+uatragada@users.noreply.github.com>
2023-04-17 20:44:44 -0400

    Adding Postgres Jar and Connection to DB

--- BeSocial.java
+++ BeSocial.java
@@ -6,7 +6,6 @@ import java.lang.*;
 public class BeSocial{
 
     private Connection connection;
-	private Properties properties;
 	private Statement statement;
 	private String url;
 	private String username;
@@ -14,7 +13,34 @@ public class BeSocial{
 
     public BeSocial(){
         Scanner input = new Scanner(System.in);
+        System.out.println("<-----LOG INTO DB----->");
+        
+    
+        /**
+         * Prompts the user for a URL, username, and password to connect to a database.
+         * If the URL is "localhost", it is converted to a PostgreSQL URL.
+         *
+         */
+        System.out.print("URL:");
+        url = input.next();
+        if(url.equals("localhost"))
+        {
+            url = "jdbc:postgresql://localhost:5432/";
+        }
+        
+        System.out.print("Username:");
+        username = input.next();
+        
+        System.out.print("Password:");
+        password = input.next();
 
+        try{
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public int createProfile(String name, String email, String password, LocalDate dateOfBirth) {
