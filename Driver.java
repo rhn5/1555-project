550ac1808189e9a0547acd8b43db0a8ca51b0dd0
uatragada <42359216+uatragada@users.noreply.github.com>
2023-04-20 19:39:46 -0400

    Stuffz

--- Driver.java
+++ Driver.java
@@ -36,76 +36,19 @@ public class Driver {
     }
     public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
         BeSocial driver = new BeSocial();
-
-        //drop the profile for driver@gmail.com if it already exists. If not, fails
-        driver.dropProfile("driver@gmail.com","yes");
-
-        //creating user profile for driver@gmail.com
-        driver.createProfile("test","driver@gmail.com" , "Datadata123", Date.valueOf("2000-01-01"));
-        //showing that if run again it fails
-        driver.createProfile("test","driver@gmail.com" , "Datadata123", Date.valueOf("2000-01-01"));
-
-        //drop the profile for driver@gmail.com, should be successful
-        driver.dropProfile("driver@gmail.com","yes");
-
-        //creating user profile for driver@gmail.com again
-        driver.createProfile("test","driver@gmail.com" , "Datadata123", Date.valueOf("2000-01-01"));
-
-        //login the driver
-        driver.login("driver@gmail.com", "Datadata123");
-
-        //send friend request to userID 10
-        driver.initiateFriendship(10);
-
-        //Group Creation
-        driver.createGroup("driverGroup", "Driver Group", 10);
-
-        //User request to join groupID = 1
-        driver.initiateAddingGroup(1, "You have been invited to join the group");
-
-        //Driver leaves driverGroup
-        driver.leaveGroup("driverGroup");
-
-        //testing search Profile
-        driver.searchForProfile("a");
-
-        //sending a message
-        String message = "Ping";
-        driver.sendMessageToUser("NishaParmar@gmail.com", message);
-        driver.sendMessageToUser("NishaParmar@gmail.com", message);
-        driver.sendMessageToUser("NishaParmar@gmail.com", message);
-        driver.sendMessageToUser("NishaParmar@gmail.com", message);
-        driver.sendMessageToUser("NishaParmar@gmail.com", message);
-
-        //Group Creation
-        driver.createGroup("driverGroup2", "Driver Group2", 10);
         
-        //Testing message to group
-        driver.sendMessageToGroup("driverGroup2");
+        driver.login("admin@besocial.com", "admin");
+        
+        for(int i = 0; i < 100; i++)
+        {
+            driver.createProfile("test", "test214"+i+"@gmail.com", "21401417abAQW", Date.valueOf("2000-01-02"));
+        }
+        for(int i = 0; i < 100; i++)
+        {
+            driver.dropProfile("test214"+i+"@gmail.com", "yes");
+        }
 
-        //Display Messages
-        driver.displayMessages();
-
-        //Display New Messages
-        driver.displayNewMessages();
-
-        //Test Rank Groups
-        driver.rankGroups();
-
-        //Test Rank Profiles
-        driver.rankProfiles();
-
-        //Testing top Messages
-        driver.topMessages(10,10);
-
-        //Test Three Degrees
-        driver.threeDegrees(10);
-
-        //Test Logout
-        driver.logout();
-
-        //Test Application exit
-        driver.exit();
+        
     }
 
     
